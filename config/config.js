afe391fb48833687cda131b0dddf4768b8d4e54f
Aryan Sharma <aryan@smartsense.co.in>
2019-05-11 13:40:32 +0530

    Added F1, and local city

--- config/config.js
+++ config/config.js
@@ -9,101 +9,96 @@
  */
 
 var config = {
-	address: "localhost", // Address to listen on, can be:
-	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-	                      // - another specific IPv4/6 to listen on a specific interface
-	                      // - "", "0.0.0.0", "::" to listen on any interface
-	                      // Default, when address config is left out, is "localhost"
-	port: 8080,
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
-	                                                       // or add a specific IPv4 of 192.168.1.5 :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+  address: "localhost", // Address to listen on, can be:
+  // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+  // - another specific IPv4/6 to listen on a specific interface
+  // - "", "0.0.0.0", "::" to listen on any interface
+  // Default, when address config is left out, is "localhost"
+  port: 8080,
+  ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+  // or add a specific IPv4 of 192.168.1.5 :
+  // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+  // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+  // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
-	language: "en",
-	timeFormat: 24,
-	units: "metric",
+  language: "en",
+  timeFormat: 24,
+  units: "metric",
 
-	modules: [
-		{
-			module: "alert",
-		},
-		{
-			module: "updatenotification",
-			position: "top_bar"
-		},
-		{
-			module: "clock",
-			position: "top_left"
-		},
-		{
-			module: "calendar",
-			header: "US Holidays",
-			position: "top_left",
-			config: {
-				calendars: [
-					{
-						symbol: "calendar-check",
-						url: "webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics"
-					}
-				]
-			}
-		},
-		{
-			module: "compliments",
-			position: "lower_third"
-		},
-		{
-			module: "currentweather",
-			position: "top_right",
-			config: {
-				location: "New York",
-				locationID: "",  //ID from http://bulk.openweathermap.org/sample/; unzip the gz file and find your city
-				appid: "YOUR_OPENWEATHER_API_KEY"
-			}
-		},
-		{
-			module: "weatherforecast",
-			position: "top_right",
-			header: "Weather Forecast",
-			config: {
-				location: "New York",
-				locationID: "5128581",  //ID from https://openweathermap.org/city
-				appid: "YOUR_OPENWEATHER_API_KEY"
-			}
-		},
-		{
-			module: "newsfeed",
-			position: "bottom_bar",
-			config: {
-				feeds: [
-					{
-						title: "New York Times",
-						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-					}
-				],
-				showSourceTitle: true,
-				showPublishDate: true
-			}
-		},
-		{
-			module: 'MMM-AlexaOnOff',
-			config: {
-				name: "mirror",
-				on: { 
-                			notification: "ONSCREENMENU_PROCESS_ACTION",
-                			payload: { actionName:'monitorOn' }
-              			},
-              			off: { 
-               				notification: "ONSCREENMENU_PROCESS_ACTION",
-                			payload: { actionName:'monitorOff' }
-              			},
-			}	
-		},
-	]
+  modules: [
+    {
+      module: "alert",
+    },
+    {
+      module: "updatenotification",
+      position: "top_bar"
+    },
+    {
+      module: "clock",
+      position: "top_left"
+    },
+    {
+      module: "calendar",
+      header: "Formula 1 Schedule",
+      position: "top_left",
+      config: {
+        calendars: [
+          {
+            symbol: ' flag-checkered',
+            url: 'http://localhost:8080/MMM-Formula1/schedule.ics',
+          }
+        ]
+      }
+    },
+    {
+      module: "compliments",
+      position: "lower_third"
+    },
+    {
+      module: "currentweather",
+      position: "top_right",
+      config: {
+        location: "Gurgaon",
+        locationID: "1270642",  //ID from http://bulk.openweathermap.org/sample/; unzip the gz file and find your city
+        appid: "62875a20c33f422148f0d5cb33404d85"
+      }
+    },
+    {
+      module: "weatherforecast",
+      position: "top_right",
+      header: "Weather Forecast",
+      config: {
+        location: "Gurgaon",
+        locationID: "1270642",  //ID from https://openweathermap.org/city
+        appid: "62875a20c33f422148f0d5cb33404d85"
+      }
+    },
+    {
+      module: "newsfeed",
+      position: "bottom_bar",
+      config: {
+        feeds: [
+          {
+            title: "NDTV",
+            url: "http://feeds.feedburner.com/NDTV-LatestNews"
+          }
+        ],
+        showSourceTitle: true,
+        showPublishDate: true
+      }
+    },
+    {
+      module: 'MMM-Formula1',
+      position: 'bottom_right',
+      header: 'F1 Standings',
+      config: {
+        calendar: true,
+        // Optional configuration options - see https://github.com/ianperrin/MMM-Formula1#configuration-options
+      }
+    }
+  ]
 
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {module.exports = config;}
+if (typeof module !== "undefined") { module.exports = config; }
